054661760470e3294ad1717edf16ce7700723b83
pav5000 <pav5000@gmail.com>
2022-08-06 20:32:21 +0300

    String utils

---
+++ stringutil/string_utils.go
@@ -0,0 +1,25 @@
+package stringutil
+
+import (
+	"math/rand"
+	"strings"
+)
+
+const (
+	LowercaseLatinLetters   = "abcdefghijklmnopqrstuvwxyz"
+	UppercaseLatinLetters   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	LatinCharacters         = LowercaseLatinLetters + UppercaseLatinLetters
+	Digits                  = "0123456789"
+	LatinCaractersAndDigits = LatinCharacters + Digits
+)
+
+// RandomASCIIString generates a random string from characters which present in the provided charset
+// only 1-byte characters are supported
+func RandomASCIIString(charset string, length int) string {
+	res := strings.Builder{}
+	res.Grow(length)
+	for i := 0; i < length; i++ {
+		res.WriteByte(charset[rand.Intn(len(charset))])
+	}
+	return res.String()
+}
