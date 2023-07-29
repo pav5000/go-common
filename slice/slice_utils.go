81ed73211580ee4238e8e40e092bdb21ff4c180e
pav5000 <pav5000@gmail.com>
2023-07-29 14:42:30 +0300

    added slice utils

---
+++ slice/slice_utils.go
@@ -0,0 +1,14 @@
+package slice
+
+import "math/rand"
+
+// RandomElement selects a random element from the slice
+// returns default value if slice is empty
+// don't use in cryptographic applications (using math/rand here)
+func RandomElement[T any](slice []T) T {
+	if len(slice) == 0 {
+		var elem T
+		return elem
+	}
+	return slice[rand.Intn(len(slice))]
+}
